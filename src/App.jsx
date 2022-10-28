98dc3f953af1afb44bce92a5586e688984d17e6b
Edward Tanguay (windows) <edward@tanguay.info>
2022-10-28 11:13:49 +0200

    bikeRentals 0

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.scss'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [bikeRentals, setBikeRentals] = useState([]);
 
   return (
     <div className="App">
     <h1>Bike Rental App</h1>
-    <p>this is it.</p>
+      <p>There are {bikeRentals.length} bike rentals.</p>
     </div>
   )
 }
