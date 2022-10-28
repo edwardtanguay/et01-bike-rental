3e5b17f930ca88a872ebf556bdeb3995f5a4ef1c
Edward Tanguay (windows) <edward@tanguay.info>
2022-10-28 11:37:28 +0200

    pigeon maps installed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import './App.scss';
 import axios from 'axios';
+import { Map, Marker } from 'pigeon-maps';
 
 const bikeRentalUrl = 'http://api.citybik.es/v2/networks';
 
@@ -18,9 +19,18 @@ function App() {
 		<div className="App">
 			<h1>Bike Rental App</h1>
 			<p>There are {bikeRentals.length} bike rentals.</p>
+			<h2>pigeon maps</h2>
+			<Map height={300} defaultCenter={[50.879, 4.6997]} defaultZoom={11}>
+				<Marker width={50} anchor={[50.879, 4.6997]} />
+			</Map>
+			<h2>all bikes</h2>
 			<div className="bikeRentals">
 				{bikeRentals.map((bikeRental, i) => {
-          return <div className="bikeRental" key={i}>{bikeRental.name} {bikeRental.location.city}</div>;
+					return (
+						<div className="bikeRental" key={i}>
+							{bikeRental.name} {bikeRental.location.city}
+						</div>
+					);
 				})}
 			</div>
 		</div>
