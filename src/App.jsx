1127922cce3a4e693464f022c16767acc4c78ade
Edward Tanguay (windows) <edward@tanguay.info>
2022-10-28 11:20:42 +0200

    have number of bike rentals

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,26 @@
-import { useState, useEffect } from 'react'
-import './App.scss'
+import { useState, useEffect } from 'react';
+import './App.scss';
+import axios from 'axios';
+
+const bikeRentalUrl = 'http://api.citybik.es/v2/networks';
 
 function App() {
-  const [bikeRentals, setBikeRentals] = useState([]);
+	const [bikeRentals, setBikeRentals] = useState([]);
 
-  return (
-    <div className="App">
-    <h1>Bike Rental App</h1>
-      <p>There are {bikeRentals.length} bike rentals.</p>
-    </div>
-  )
+  useEffect(() => {
+    (async () => {
+      const _bikeRentals = (await axios.get(bikeRentalUrl)).data.networks;
+      console.log(_bikeRentals);
+      setBikeRentals(_bikeRentals);
+    })();
+  }, []);
+
+	return (
+		<div className="App">
+			<h1>Bike Rental App</h1>
+			<p>There are {bikeRentals.length} bike rentals.</p>
+		</div>
+	);
 }
 
-export default App
+export default App;
