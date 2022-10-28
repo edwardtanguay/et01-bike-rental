eacf9bdfe96a1ea58af4802301cdbdd39a146284
Edward Tanguay (windows) <edward@tanguay.info>
2022-10-28 11:22:24 +0200

    showing nnn

--- src/App.jsx
+++ src/App.jsx
@@ -7,18 +7,22 @@ const bikeRentalUrl = 'http://api.citybik.es/v2/networks';
 function App() {
 	const [bikeRentals, setBikeRentals] = useState([]);
 
-  useEffect(() => {
-    (async () => {
-      const _bikeRentals = (await axios.get(bikeRentalUrl)).data.networks;
-      console.log(_bikeRentals);
-      setBikeRentals(_bikeRentals);
-    })();
-  }, []);
+	useEffect(() => {
+		(async () => {
+			const _bikeRentals = (await axios.get(bikeRentalUrl)).data.networks;
+			setBikeRentals(_bikeRentals);
+		})();
+	}, []);
 
 	return (
 		<div className="App">
 			<h1>Bike Rental App</h1>
 			<p>There are {bikeRentals.length} bike rentals.</p>
+			<div className="bikeRentals">
+				{bikeRentals.map((bikeRental, i) => {
+					return <div className="bikeRental" key={i}>nnn</div>;
+				})}
+			</div>
 		</div>
 	);
 }
